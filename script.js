701139de0172eb71be10b781e45b669b772700dc
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-11 09:57:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,4 +13,7 @@ window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
+window.team1[0]="kishan";
 window.cap1 = cap1;
+window.cap1.name="kishan";
+window.cap1["age"]=22;
