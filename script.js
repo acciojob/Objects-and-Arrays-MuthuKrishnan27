c139e71563e79232df6f189e070a36148ddb035a
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-19 19:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-let team = players, team1 = [...players];
-let cap1 = {...person};
+const team = players;
+const team1 = players.slice();
+
+//let copiedPerson = Object.assign({}, person);
+let cap1 = Object.assign({}, person);
+
 
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
-// window.team1[0]="kishan";
-// window.cap1 = cap1;
-// window.cap1.name:"kishan";
-// window.cap1["age"]:22;
